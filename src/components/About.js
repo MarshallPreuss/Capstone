3ae9fb727e9701b53bd069f77e0ce042a3c22b18
MarshallPreuss <marshallpreuss@gmail.com>
2020-09-18 08:20:08 -0400

    Commit

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const About = () => {
+return(
+    <div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">About Us</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Our Website allows easy access to buy animal produts from toys to food to luxery items for your 
+              pets everyday needs.
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+}
