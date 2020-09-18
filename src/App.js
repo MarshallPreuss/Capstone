7abd45bc001e326231ed5b76f73846afb04692f4
MarshallPreuss <marshallpreuss@gmail.com>
2020-09-18 08:30:53 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@ import React from 'react';
 import Dashboard from './Pages/Dashboard'
 import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-
-import About from './components/About'
-
 function App() {
   return (
     <div className="App">
-      <Router>
-
-      <Switch>
-        <Route exact path='/' component={Dashboard}/>
-
-        <Route exact path='/about' component={About} />
-
-        <Redirect to='/' />
-      </Switch>
-    </Router>
+      <Dashboard/>
     </div>
   );
 }
